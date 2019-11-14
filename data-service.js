ab7733f5e84c978520869840fb1425f6899d595f
cre8ivelab <praranjit1997@gmail.com>
2019-11-14 10:36:23 -0500

    sequelize model added

--- data-service.js
+++ data-service.js
@@ -1,3 +1,44 @@
+const Sequelize = require('sequelize');
+var sequelize = new Sequelize('d4e89ai9iepn6e', 'tmmanuqjjsawji', 'f4109dc16690cc4b7655a811e181deb96ec14cfd4f752b2f18be331e753b8d68', {
+    host: 'ec2-174-129-252-240.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+        ssl: true
+    }
+});
+
+//employees sequelize model
+var Employee = sequelize.define('Employee',{
+    employeeNum: {
+        type: Sequelize.INTERGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    email: Sequelize.STRING,
+    SSN: Sequelize.STRING,
+    addressStreet: Sequelize.STRING,
+    addressCity: Sequelize.STRING,
+    addressState: Sequelize.STRING,
+    addressPostal: Sequelize.STRING,
+    maritalStatus: Sequelize.STRING,
+    isManager: Sequelize.BOOLEAN,
+    employeeManagerNum: Sequelize.INTEGER,
+    status: Sequelize.STRING,
+    hireDate: Sequelize.STRING
+});
+
+//Department sequelize model
+var Department = sequelize.define('Department', {
+    departmentId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    departmentName: Sequelize.STRING
+});
 
 
 //set a promise
